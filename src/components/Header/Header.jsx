7fc8528a5d8b1e65adf6a7df002b653c10b8d552
mertayda <mertayda@outlook.com>
2024-07-21 05:10:17 +0300

    ...

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,48 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "../../../Assets/mile1-assets/logo.webp";
 import { Link } from "react-router-dom";
 import "remixicon/fonts/remixicon.css";
 
 const Header = () => {
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-  // Toggle mobile menu
-  const toggleMobileMenu = () => {
-    setMobileMenuOpen(!isMobileMenuOpen);
-  };
-
   return (
-    <header className="bg-[#CE2829] min-w-min">
+    <header className="bg-[#FDC913]">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between py-4">
+        <div className="w-full flex items-center justify-between py-4 text-black">
           <div className="w-56">
             <Link to="/">
               <img src={logo} alt="Company Logo" className="object-cover" />
             </Link>
           </div>
-          <nav className="w-full text-center justify-center hidden sm:flex text-lg  space-x-3 md:space-x-12  mx-auto">
+          <nav className="w-full text-center justify-center hidden sm:flex text-2xl space-x-3 md:space-x-12 mx-auto text-black">
             <Link
               to="/"
-              className="text-white no-underline font-bold font-satisfty"
+              className="text-white no-underline font-bold font-satisfty hover:text-yellow-500"
             >
               Anasayfa
             </Link>
             <Link
               to="/about"
-              className="text-white no-underline font-bold font-satisfty"
+              className="text-white no-underline font-bold font-satisfty hover:text-yellow-500"
             >
               Yemekler
             </Link>
             <Link
               to="/products"
-              className="text-white no-underline font-bold font-satisfty"
+              className="text-white no-underline font-bold font-satisfty hover:text-yellow-500"
               aria-label="My Products"
             >
               Ürünlerim
             </Link>
             <Link
               to="/contact"
-              className="text-white no-underline font-bold font-satisfty"
+              className="text-white no-underline font-bold font-satisfty hover:text-yellow-500"
               aria-label="Contact"
             >
               İletişim
@@ -53,7 +46,8 @@ const Header = () => {
               className="relative cursor-pointer"
               aria-label="Shopping Basket"
             >
-              <i className="ri-shopping-basket-line text-white text-lg"></i>
+              <i className="ri-shopping-basket-line text-white text-2xl"></i>{" "}
+              {/* Increase font size here */}
               <span className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">
                 0
               </span>
@@ -61,16 +55,17 @@ const Header = () => {
             <Link
               to="/login"
               aria-label="User Login"
-              className="text-white no-underline"
+              className="text-white no-underline hover:text-yellow-500"
             >
-              <i className="ri-user-follow-fill text-lg"></i>
+              <i className="ri-user-follow-fill text-2xl"></i>{" "}
+              {/* Increase font size here */}
             </Link>
             <button
-              onClick={toggleMobileMenu}
               className="sm:hidden text-white focus:outline-none"
               aria-label="Toggle Mobile Menu"
             >
-              <i className="ri-menu-line text-lg"></i>
+              <i className="ri-menu-line text-2xl"></i>{" "}
+              {/* Increase font size here */}
             </button>
           </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,106 @@
 import React from "react";
 import image from "../../Assets/mile1-assets/home-banner.png";
 import { Link } from "react-router-dom";
+import Category from "../components/UI/category/Category";
+import featureImg01 from "../../Assets/mile1-assets/images/service-01.png";
+import featureImg02 from "../../Assets/mile1-assets/images/service-02.png";
+import featureImg03 from "../../Assets/mile1-assets/images/service-03.png";
+
+const featureData = [
+  {
+    title: "Hızlı Teslimat",
+    imgUrl: featureImg01,
+    desc: "Siparişiniz, sıcak ve taze olarak kapınıza gelsin diye hızlı bir şekilde hazırlanıyor..",
+  },
+
+  {
+    title: "Süper Lezzet",
+    imgUrl: featureImg02,
+    desc: "Yerel tedarikçilerden aldığımız en taze malzemelerle oluşturduğumuz pizzalar.",
+  },
+  {
+    title: "Kolay Sipariş",
+    imgUrl: featureImg03,
+    desc: "Her pizza, özel tariflerimizle hazırlanıyor; sizi sıradanlıktan kurtarıyor!",
+  },
+];
+
 const Home = () => {
   return (
-    <div
-      className="w-full h-screen flex flex-col text-white"
-      style={{
-        backgroundImage: `url(${image})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundColor: "#CE2829",
-      }}
-    >
-      <div className="flex flex-col items-center justify-center content-center mt-8 bg-opacity-90 p-5 rounded-lg">
-        <h1 className="text-3xl sm:text-4xl lg:text-5xl mt-4 font-quattrocento font-semibold text-center">
-          Teknolojik Lezzetler
-        </h1>
-        <div className="flex flex-col text-center justify-center items-center mt-6">
-          <span className="text-[#FDC913] text-3xl font-satisfty">
-            Fırsatı Kaçırma
-          </span>
-          <h1 className="text-xl sm:text-2xl lg:text-4xl font-satisfty">
-            KOD ACIKTIRIR
+    <>
+      <div
+        className="w-full h-screen flex flex-col text-white"
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: "#CE2829",
+        }}
+      >
+        <div className="flex flex-col items-center justify-center content-center mt-8 bg-opacity-90 p-5 rounded-lg">
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl mt-4 font-quattrocento font-semibold text-center">
+            Teknolojik Lezzetler
           </h1>
+          <div className="flex flex-col text-center justify-center items-center mt-6">
+            <span className="text-[#FDC913] text-3xl font-satisfty">
+              Fırsatı Kaçırma
+            </span>
+            <h1 className="text-xl sm:text-2xl lg:text-4xl font-satisfty">
+              KOD ACIKTIRIR
+            </h1>
 
-          <h1 className="text-xl sm:text-2xl lg:text-4xl font-satisfty">
-            PİZZA DOYURUR
-          </h1>
-          <div className="mt-6">
-            <Link to="/order">
-              <button
-                type="button"
-                className="py-2 px-8 fonmt-quattrocento inline-flex items-center gap-x-2 text-md font-semibold rounded-3xl border border-transparent bg-[#FDC913] text-black hover:bg-yellow-600 disabled:opacity-50 disabled:pointer-events-none"
-              >
-                ACIKTIM
-              </button>
-            </Link>
+            <h1 className="text-xl sm:text-2xl lg:text-4xl font-satisfty">
+              PİZZA DOYURUR
+            </h1>
+            <div className="mt-6">
+              <Link to="/order">
+                <button
+                  type="button"
+                  className="py-2 px-8 fonmt-quattrocento inline-flex items-center gap-x-2 text-md font-semibold rounded-3xl border border-transparent bg-[#FDC913] text-black hover:bg-yellow-600 disabled:opacity-50 disabled:pointer-events-none"
+                >
+                  ACIKTIM
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Category></Category>
+      <section className="py-10">
+        <div className="container mx-auto">
+          <div className="text-center">
+            <h5 className="text-red-600 mb-4">Ne Sunuyoruz?</h5>
+            <h2 className="text-2xl font-bold">
+              Evde Dinlenin, Lezzeti Kapınıza Getirelim!
+            </h2>
+            <h2 className="text-2xl font-bold">
+              Acıktınız mı? <span className="text-red-600">Biz hallederiz</span>
+            </h2>
+            <p className="mb-1 mt-4 text-gray-600">
+              Hayalinizdeki pizza, sadece bir tık uzağınızda! Her bir pizzamız,
+              taze ve kaliteli malzemelerle, ustalıkla hazırlanıyor. Düşük
+              kalorili diyet mi, klasik lezzetler mi yoksa yeni tatlar mı
+              arıyorsunuz? Her damak tadına uygun bir seçenekle, pizzayı yeniden
+              tanımlıyoruz!
+            </p>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 mt-5 gap-4">
+            {featureData.map((item, index) => (
+              <div key={index} className="mt-5 p-4 text-center  rounded-lg">
+                <img
+                  src={item.imgUrl}
+                  alt="feature-img"
+                  className="w-1/4 mb-3 mx-auto"
+                />
+                <h5 className="font-bold mb-3">{item.title}</h5>
+                <p className="text-gray-600">{item.desc}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
